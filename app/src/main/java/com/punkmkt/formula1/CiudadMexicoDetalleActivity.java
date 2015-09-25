f9505c50919f266e502345bf673e7bcbfabcccb8
germanpunk <german@punkmkt.com>
2015-09-25 00:12:24 -0400

    Ciudad de mexico 2v

--- app/src/main/java/com/punkmkt/formula1/CiudadMexicoDetalleActivity.java
+++ app/src/main/java/com/punkmkt/formula1/CiudadMexicoDetalleActivity.java
@@ -86,12 +86,10 @@ public class CiudadMexicoDetalleActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
     public void MostrarHoteles(View v){
