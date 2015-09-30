d30b8063c50761ede8fa21fc637c2050ff02864e
germanpunk <german@punkmkt.com>
2015-09-30 00:14:50 -0400

    Avance Login Facebook

--- app/src/main/java/com/punkmkt/formula1/LoginSingUpActivity.java
+++ app/src/main/java/com/punkmkt/formula1/LoginSingUpActivity.java
@@ -2,18 +2,31 @@ package com.punkmkt.formula1;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
@@ -22,6 +35,8 @@ import com.parse.SignUpCallback;
 import com.punkmkt.formula1.adapters.CustomizedSpinnerAdapter;
 import com.punkmkt.formula1.fragments.LoginFBFragment;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
@@ -31,20 +46,55 @@ public class LoginSingUpActivity extends FragmentActivity {
     private EditText emailView;
     private EditText zonaView;
     private EditText asientoView;
+    private Spinner genero;
+    private TextView name;
+    private TextView facebook_id;
+    private TextView locale;
+    private TextView link;
+    private TextView age_range;
+    CallbackManager callbackManager;
+    private ParseObject customUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_sing_up);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new LoginFBFragment()).commit();
         }
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        Log.d("ohhh", loginResult.getAccessToken().toString());
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+        showHashKey(getApplicationContext());
+
         first_nameView = (EditText) findViewById(R.id.first_name);
         last_nameView = (EditText) findViewById(R.id.last_name);
         emailView = (EditText) findViewById(R.id.email);
         zonaView = (EditText) findViewById(R.id.zona);
         asientoView = (EditText) findViewById(R.id.asiento);
+        name = (TextView) findViewById(R.id.name);
+        facebook_id = (TextView) findViewById(R.id.facebook_id);
+        locale = (TextView) findViewById(R.id.locale);
+        link = (TextView) findViewById(R.id.link);
+        age_range = (TextView) findViewById(R.id.age_range);
+         genero = (Spinner) findViewById(R.id.spinner_gender);
 
-        Spinner mySpinner = (Spinner) findViewById(R.id.spinner_gender);
         final String[] data_array = getResources().getStringArray(R.array.gender_arrays);
 
         final ArrayList<String> data = new ArrayList<String>();
@@ -56,7 +106,7 @@ public class LoginSingUpActivity extends FragmentActivity {
         final ArrayAdapter<String> adapter1 = new CustomizedSpinnerAdapter(
                 LoginSingUpActivity.this, android.R.layout.simple_spinner_item,
                 data);
-        mySpinner.setAdapter(adapter1);
+        genero.setAdapter(adapter1);
 
 
 
@@ -88,6 +138,11 @@ public class LoginSingUpActivity extends FragmentActivity {
                     validationErrorMessage.append(getResources().getString(R.string.error_blank_asiento));
                 }
 
+                if(!ValidateSpinner(genero,"Género")){
+                    validationError = true;
+                    validationErrorMessage.append(getResources().getString(R.string.error_blank_genero));
+                }
+
                 validationErrorMessage.append(getResources().getString(R.string.error_end));
 
                 // If there is a validation error, display the error
@@ -104,13 +159,20 @@ public class LoginSingUpActivity extends FragmentActivity {
                 dlg.show();
 
 
-                ParseObject customUser = new ParseObject("CustomUser");
+                customUser = new ParseObject("CustomUser");
+
                 //customUser.put("foo", "bar");
                 customUser.put("first_name", first_nameView.getText().toString());
                 customUser.put("last_name", last_nameView.getText().toString());
                 customUser.put("email",emailView.getText().toString());
+                customUser.put("gender", genero.getSelectedItem().toString());
+                customUser.put("name",name.getText().toString());
+                customUser.put("facebook_id",facebook_id.getText().toString());
+                customUser.put("locale",locale.getText().toString());
+                customUser.put("link",link.getText().toString());
+                customUser.put("age_range",age_range.getText().toString());
                 customUser.put("zona",zonaView.getText().toString());
-                customUser.put("asiento",asientoView.getText().toString());
+                customUser.put("asiento", asientoView.getText().toString());
 
                 // Call the Parse signup method
                 customUser.saveInBackground(new SaveCallback() {
@@ -119,9 +181,13 @@ public class LoginSingUpActivity extends FragmentActivity {
                     public void done(ParseException e) {
                         dlg.dismiss();
                         if (e != null) {
+
                             // Show the error message
                             Toast.makeText(LoginSingUpActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+
+                            Log.d("parse",customUser.getObjectId().toString());
                         } else {
+                            Log.d("parse",customUser.getObjectId().toString());
                             // Start an intent for the dispatch activity
                             Intent intent = new Intent(LoginSingUpActivity.this, MainActivity.class);
                             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -129,6 +195,8 @@ public class LoginSingUpActivity extends FragmentActivity {
                         }
                     }
                 });
+
+
             }
         });
     }
@@ -178,5 +246,37 @@ public class LoginSingUpActivity extends FragmentActivity {
             return false;
 
         return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+    public static void showHashKey(Context context) {
+
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(
+                    "com.punkmkt.formula1", PackageManager.GET_SIGNATURES); //Your            package name here
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+
+                Log.i("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+        } catch (NoSuchAlgorithmException e) {
+        }
+    }
+
+    public final static boolean ValidateSpinner(Spinner s,String s_option){
+        String st =s.getSelectedItem().toString();
+        int pos =s.getSelectedItemPosition();
+        if(pos==0)
+        {
+            return false;
+        }
+        if(!st.equals(s_option))
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
